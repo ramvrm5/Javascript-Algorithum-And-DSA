3a425e95847847829603756f55cb7b22496a3e6c
ram verma <ramvrm5@gmail.com>
2023-09-28 10:23:10 +0530

    two new patterns anagram, multiple pointers

--- Frequency Pattern/frequency-Pattern-Anagram.js
+++ Frequency Pattern/frequency-Pattern-Anagram.js
@@ -2,12 +2,15 @@ function validAnagram(first, second) {
     if (first.length !== second.length) {
         return false;
     }
+
     const lookup = {};
+
     for (let i = 0; i < first.length; i++) {
         let letter = first[i];
         //if letter exists, increment, otherwise set to 1
         lookup[letter] ? lookup[letter] += 1 : lookup[letter] = 1;
     }
+
     for (let i = 0; i < second.length; i++) {
         let letter = second[i];
         //can't find letter or letter is zero then it's not an anagram
@@ -17,6 +20,7 @@ function validAnagram(first, second) {
             lookup[letter] -= 1;
         }
     }
+
     return true;
 }
 
